ebc21893e79fb21bdf37a571f1df28b4309a8721
adamolek <kurzoj@onet.eu>
2016-05-19 18:22:34 +0200

    creation entries in /sys/class added

--- modules/pcf8574/with_devicetree/pcf8574.c
+++ modules/pcf8574/with_devicetree/pcf8574.c
@@ -3,12 +3,22 @@
 #include <linux/slab.h>
 #include <linux/i2c.h>
 #include <linux/sysfs.h>
+#include <linux/fs.h>
 #include <linux/hwmon-sysfs.h>
 
 #define PCF8574 "pcf8574sample"
 #define DRIVER "PCF8574SAMPLE"
 
 //driver needs device tree entry in order to work, overlay need to be loaded
+
+struct pcf8574_device
+{
+	dev_t cdev;
+	struct device *dev;
+	char name[16];
+};
+
+static struct class *pcf8574_class;
 
 static ssize_t pins_state_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
@@ -61,19 +71,62 @@ static const struct attribute_group pcf8574_attr_group = {
 static int pcf8574_probe(struct i2c_client *client, const struct i2c_device_id *id)
 {
 	int err;
-	err = device_create_file(&client->dev, &dev_attr_pins_state);
+	struct pcf8574_device *pcf8574_dev;
+	static int number = 0;
+
+	pcf8574_dev = kzalloc(sizeof(struct pcf8574_device), GFP_KERNEL);
+	if(!pcf8574_dev)
+	{
+		printk(KERN_ERR "%s: %s: cannot allocate memory\n", DRIVER, __func__);
+		return -1;
+	}
+
+	sprintf(pcf8574_dev->name, "pcf8574_%i", number++);
+	err = alloc_chrdev_region(&pcf8574_dev->cdev, 0, 1, pcf8574_dev->name);
+	if(err)
+	{
+		printk(KERN_ERR "%s: %s: alloc_chrdev_region failed(%i)\n", DRIVER, __func__, err);
+		goto err1;
+	}
+
+	pcf8574_dev->dev = device_create(pcf8574_class, &client->dev, pcf8574_dev->cdev, NULL, pcf8574_dev->name);
+	if(IS_ERR(pcf8574_dev->dev))
+	{
+		printk(KERN_ERR "%s: %s: cannot create device\n", DRIVER, __func__);
+		err = -1;
+		goto err2;
+	}
+
+	err = device_create_file(pcf8574_dev->dev, &dev_attr_pins_state);
 	if(err)
 	{
 		printk(KERN_ERR "%s: %s: cannot create sysfs entry(%i)\n", DRIVER, __func__, err);
-		return err;
+		goto err3;
 	}
+
+	i2c_set_clientdata(client, pcf8574_dev);
+
 	printk(KERN_INFO "%s: %s: device address 0x%X\n", DRIVER, __func__, client->addr);
 	return 0;
+
+	err3:
+	device_destroy(pcf8574_class, pcf8574_dev->cdev);
+	err2:
+	unregister_chrdev_region(pcf8574_dev->cdev, 1);
+	err1:
+	kfree(pcf8574_dev);
+	return err;
 }
 
 static int pcf8574_remove(struct i2c_client *client)
 {
-	device_remove_file(&client->dev, &dev_attr_pins_state);
+	struct pcf8574_device *pcf8574_dev = i2c_get_clientdata(client);
+
+	device_remove_file(pcf8574_dev->dev, &dev_attr_pins_state);
+	device_destroy(pcf8574_class, pcf8574_dev->cdev);
+	unregister_chrdev_region(pcf8574_dev->cdev, 1);
+	kfree(pcf8574_dev);
+	i2c_set_clientdata(client, NULL);
 	printk(KERN_INFO "%s: %s: device address 0x%X\n", DRIVER, __func__, client->addr);
 	return 0;
 }
@@ -97,10 +150,18 @@ static int __init pcf8574_init(void)
 {
 	int err;
 
+	pcf8574_class = class_create(THIS_MODULE, PCF8574);
+	if(!pcf8574_class)
+	{
+		printk(KERN_ERR "%s: %s: cannot create class\n", DRIVER, __func__);
+		return -1;
+	}
+
 	err = i2c_add_driver(&pcf8574_i2c_driver);
 	if(err)
 	{
 		printk(KERN_ERR "%s: %s: cannot add i2c driver(%i)\n", DRIVER, __func__, err);
+		class_destroy(pcf8574_class);
 		return err;
 	}
 
@@ -110,6 +171,7 @@ static int __init pcf8574_init(void)
 static void __exit pcf8574_exit(void)
 {
 	i2c_del_driver(&pcf8574_i2c_driver);
+	class_destroy(pcf8574_class);
 }
 
 module_init(pcf8574_init);
